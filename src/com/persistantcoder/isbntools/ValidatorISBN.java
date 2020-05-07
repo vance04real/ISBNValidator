8d842ff5570d2d2324173989b6bf22c55bfaff7b
Evans K F Chikuni <vance04real@gmail.com>
2020-05-07 04:04:37 +0200

    Initial commit

---
+++ src/com/persistantcoder/isbntools/ValidatorISBN.java
@@ -0,0 +1,7 @@
+package com.persistantcoder.isbntools;
+
+/**
+ * Created by Evans K F C on May ,2020
+ **/
+public class ValidatorISBN {
+}
---
+++ test/com/persistantcoder/isbntools/ValidatorISBNTest.java
@@ -0,0 +1,18 @@
+package com.persistantcoder.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Evans K F C on May ,2020
+ **/
+public class ValidatorISBNTest {
+
+    @Test
+    public void exampleTest(){
+
+        //fail();
+    }
+
+}
