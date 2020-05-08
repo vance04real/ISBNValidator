edc9c90296d0ee54bc1379bd055ba17874fb7345
Evans K F Chikuni <vance04real@gmail.com>
2020-05-08 14:58:38 +0200

    added functionality for validating 13 digits ISBN

--- src/com/persistantcoder/isbntools/ValidatorISBN.java
+++ src/com/persistantcoder/isbntools/ValidatorISBN.java
@@ -8,25 +8,47 @@ public class ValidatorISBN {
 
     public boolean checkISBN(String isbnNumber) {
 
-        if(isbnNumber.length()!=10) throw  new NumberFormatException("ISBN numbers must be 10 digit long");
+        if (isbnNumber.length() == 13) {
+            int total = 0;
 
+            for (int i = 0; i < 13; i++) {
 
-        int total = 0;
-
-        for(int i= 0; i<10 ;i++){
-
-            if(!Character.isDigit(isbnNumber.charAt(i))) {
-                if(i==9 && isbnNumber.charAt(i)=='X'){
-                    total+=10;
-                }else{
-                    throw new NumberFormatException("ISBN number can only contain numeric digits");
+                if (i % 2 == 0) {
+                    total += Character.getNumericValue(isbnNumber.charAt(i));
+                } else {
+                    total += Character.getNumericValue(isbnNumber.charAt(i)) * 3;
                 }
-            }else{
-                total+= Character.getNumericValue(isbnNumber.charAt(i) ) * (10-i);
             }
 
+            final boolean b = total % 10 == 0 ? true : false;
+
+            return b;
+
+        } else {
+
+            if (isbnNumber.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digit long");
+
+
+            int total = 0;
+
+            for (int i = 0; i < 10; i++) {
+
+                if (!Character.isDigit(isbnNumber.charAt(i))) {
+                    if (i == 9 && isbnNumber.charAt(i) == 'X') {
+                        total += 10;
+                    } else {
+                        throw new NumberFormatException("ISBN number can only contain numeric digits");
+                    }
+                } else {
+                    total += Character.getNumericValue(isbnNumber.charAt(i)) * (10 - i);
+                }
+
+            }
+            return total % 11 == 0;
+
+
         }
-        return total % 11 == 0;
+
 
     }
 }
--- test/com/persistantcoder/isbntools/ValidatorISBNTest.java
+++ test/com/persistantcoder/isbntools/ValidatorISBNTest.java
@@ -3,6 +3,7 @@ package com.persistantcoder.isbntools;
 import org.junit.Before;
 import org.junit.Test;
 
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +21,7 @@ public class ValidatorISBNTest {
 
 
     @Test
-    public void checkAValidISBN(){
+    public void checkTenDigitValidISBN(){
         boolean result = validator.checkISBN("0140449116");
         assertTrue("first value",result);
         result = validator.checkISBN("0140177396");
@@ -29,7 +30,17 @@ public class ValidatorISBNTest {
     }
 
     @Test
-    public void ISBNsEndingInAnXAreValid(){
+    public void checkValidThirteenDigitISBNNumber(){
+
+        boolean result=  validator.checkISBN("9781118771334");
+        assertTrue(result);
+
+        result=  validator.checkISBN("9780134685991");
+        assertTrue(result);
+    }
+
+    @Test
+    public void TenDigitISBNsEndingInAnXAreValid(){
 
         boolean result = validator.checkISBN("012000030X");
         assertTrue(result);
@@ -37,7 +48,16 @@ public class ValidatorISBNTest {
     }
 
     @Test
-    public void checkAnInvalidISBN(){
+    public void checkAThirteenDigitInvalidISBN(){
+
+        boolean result = validator.checkISBN("9780134685999");
+        assertFalse(result);
+    }
+
+
+
+    @Test
+    public void checkATenDigitInvalidISBN(){
 
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
@@ -54,6 +74,7 @@ public class ValidatorISBNTest {
 
         validator.checkISBN("helloworld");
     }
+
 
 
 }
