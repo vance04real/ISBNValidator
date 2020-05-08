ecd937272bf91f089b12a35b76a4ac5a5836af39
Evans K F Chikuni <vance04real@gmail.com>
2020-05-08 10:19:31 +0200

    create method to check Valid ISBN number

--- src/com/persistantcoder/isbntools/ValidatorISBN.java
+++ src/com/persistantcoder/isbntools/ValidatorISBN.java
@@ -4,4 +4,20 @@ package com.persistantcoder.isbntools;
  * Created by Evans K F C on May ,2020
  **/
 public class ValidatorISBN {
+
+
+    public boolean checkISBN(String isbnNumber) {
+
+        if(isbnNumber.length()!=10) throw  new NumberFormatException("ISBN numbers must be 10 digit long");
+
+
+        int total = 0;
+
+        for(int i= 0; i<10 ;i++){
+
+            total+= isbnNumber.charAt(i) * (10-i);
+        }
+        return total % 11 == 0;
+
+    }
 }
--- test/com/persistantcoder/isbntools/ValidatorISBNTest.java
+++ test/com/persistantcoder/isbntools/ValidatorISBNTest.java
@@ -1,18 +1,47 @@
 package com.persistantcoder.isbntools;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Evans K F C on May ,2020
  **/
 public class ValidatorISBNTest {
 
-    @Test
-    public void exampleTest(){
+    private ValidatorISBN validator;
 
-        //fail();
+    @Before
+    public void setup(){
+        validator  = new ValidatorISBN();
+    }
+
+
+
+
+
+    @Test
+    public void checkAValidISBN(){
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value",result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value",result);
+
+    }
+
+    @Test
+    public void checkAnInvalidISBN(){
+
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test(expected=NumberFormatException.class)
+    public void nineDigitsISBNAreNotAllowed(){
+        validator.checkISBN("123456789");
+
     }
 
 }
